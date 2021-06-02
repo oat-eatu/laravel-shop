d757a094b6846307d31163fe03ab020ebfca7353
oat-eatu <huizhong197711@126.com>
2021-06-02 14:28:51 +0800

    邮箱验证中间件

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -14,9 +14,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -25,5 +23,9 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified' => 'boolean',
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/','PagesController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+
+    Route::group(['middleware' => 'email_verified'], function () {
+        Route::get('/test', function () {
+            echo "邮箱未验证";
+        });
+    });
+});
