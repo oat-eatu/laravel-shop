e1c49b082473ba4934b3fe1a4e9f84684748578f
oat-eatu <huizhong197711@126.com>
2021-06-08 12:17:33 +0800

    触发激活邮件

---
+++ app/Http/Controllers/EmailVerificationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Notifications\EmailVerificationNotification;
+use Illuminate\Http\Request;
+use \Cache;
+
+class EmailVerificationController extends Controller
+{
+    public function verify(Request $request)
+    {
+        $email = $request->email;
+        $token = $request->token;
+        if (!$email || !$token) {
+            throw  new \Exception('验证链接不正确');
+        }
+
+        if ($token != Cache::get('email_verification_' . $email)) {
+            throw new \Exception('验证链接不正确或已过期');
+        }
+
+        if (!$user = User::where('email', $email)->first()) {
+            throw new \Exception('用户不存在');
+        }
+        // 将指定的 key 从缓存中删除，由于已经完成了验证，这个缓存就没有必要继续保留。
+        Cache::forget('email_verification_' . $email);
+
+        $user->update(['email_verified' => true]);
+
+        // 最后告知用户邮箱验证成功。
+        return view('pages.success', ['msg' => '邮箱验证成功']);
+    }
+
+    //手动发送邮件
+    public function send(Request $request)
+    {
+        $user = $request->user();
+        if ($user->email_verified) {
+            throw new \Exception('您已验证过邮箱了');
+        }
+
+        // 调用 notify() 方法用来发送我们定义好的通知类
+        $user->notify(new EmailVerificationNotification());
+
+        return view('pages.success', ['msg' => '邮件发送成功']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,19 @@ Auth::routes();
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
 
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    //手动发送验证邮件
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+
+    
     Route::group(['middleware' => 'email_verified'], function () {
-        Route::get('/test', function () {
-            echo "邮箱未验证";
-        });
+
+
     });
+});
+
+
+
+Route::get('/test',function (){
+    dd(route('email_verification.verify',['name'=>'jack']));
 });
